7cb12b3000d36bce21d5d402911ab6846e81a72d
Dylan Redmon <ddredmon@iu.edu>
2019-08-28 21:53:51 -0400

    does the asssignment

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, my name is Dylan Redmon!')
+print('My favorite game is Persona 5.')
+print('The only concern I have with this class is that I might miss crucial details while trying to keep up with the exercises.')
+print('I am really excited to learn how to create games and work with a team in the game jam!')
+print('My stackoverflow.com user number is 11991363.')
+print('The URL to my github account is https://github.com/ddredmon ')
